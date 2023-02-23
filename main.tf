978312c5cee05b6b5758c1765430570db089aafa
Srikanth Chilaka <chilaka.s@northeastern.edu>
2023-02-23 11:24:14 -0500

    added assignment4

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = var.aws_region
+  region  = var.aws_region
   profile = var.aws_profile
 }
 
@@ -11,57 +11,57 @@ resource "aws_vpc" "vpc_1" {
   }
 }
 
-resource "aws_vpc" "vpc_2" {
-  cidr_block = var.vpc_cidr[1]
+# resource "aws_vpc" "vpc_2" {
+#   cidr_block = var.vpc_cidr[1]
 
-  tags = {
-    Name = "${var.vpc_name_2}"
-  }
-}
+#   tags = {
+#     Name = "${var.vpc_name_2}"
+#   }
+# }
 
 resource "aws_subnet" "public_subnets_1" {
-  count = 3
-  vpc_id = aws_vpc.vpc_1.id
-  cidr_block = "${var.subnet_prefix_1}${count.index+1}${var.subnet_postfix}"
+  count             = 3
+  vpc_id            = aws_vpc.vpc_1.id
+  cidr_block        = "${var.subnet_prefix_1}${count.index + 1}${var.subnet_postfix}"
   availability_zone = "${var.aws_region}${var.availability_zones[count.index]}"
 
   tags = {
-    Name = "${var.public_subnet_name}-${count.index+1}"
+    Name = "${var.public_subnet_name}-${count.index + 1}"
   }
 }
 
-resource "aws_subnet" "public_subnets_2" {
-  count = 3
-  vpc_id = aws_vpc.vpc_2.id
-  cidr_block = "${var.subnet_prefix_2}${count.index+1}${var.subnet_postfix}"
-  availability_zone = "${var.aws_region}${var.availability_zones[count.index]}"
+# resource "aws_subnet" "public_subnets_2" {
+#   count             = 3
+#   vpc_id            = aws_vpc.vpc_2.id
+#   cidr_block        = "${var.subnet_prefix_2}${count.index + 1}${var.subnet_postfix}"
+#   availability_zone = "${var.aws_region}${var.availability_zones[count.index]}"
 
-  tags = {
-    Name = "${var.public_subnet_name}-${count.index+1}"
-  }
-}
+#   tags = {
+#     Name = "${var.public_subnet_name}-${count.index + 1}"
+#   }
+# }
 
 resource "aws_subnet" "private_subnets_1" {
-  count = 3
-  vpc_id = aws_vpc.vpc_1.id
-  cidr_block = "${var.subnet_prefix_1}${count.index+4}${var.subnet_postfix}"
+  count             = 3
+  vpc_id            = aws_vpc.vpc_1.id
+  cidr_block        = "${var.subnet_prefix_1}${count.index + 4}${var.subnet_postfix}"
   availability_zone = "${var.aws_region}${var.availability_zones[count.index]}"
 
   tags = {
-    Name = "${var.private_subnet_name}-${count.index+1}"
+    Name = "${var.private_subnet_name}-${count.index + 1}"
   }
 }
 
-resource "aws_subnet" "private_subnets_2" {
-  count = 3
-  vpc_id = aws_vpc.vpc_2.id
-  cidr_block = "${var.subnet_prefix_2}${count.index+4}${var.subnet_postfix}"
-  availability_zone = "${var.aws_region}${var.availability_zones[count.index]}"
+# resource "aws_subnet" "private_subnets_2" {
+#   count             = 3
+#   vpc_id            = aws_vpc.vpc_2.id
+#   cidr_block        = "${var.subnet_prefix_2}${count.index + 4}${var.subnet_postfix}"
+#   availability_zone = "${var.aws_region}${var.availability_zones[count.index]}"
 
-  tags = {
-    Name = "${var.private_subnet_name}-${count.index+1}"
-  }
-}
+#   tags = {
+#     Name = "${var.private_subnet_name}-${count.index + 1}"
+#   }
+# }
 
 resource "aws_internet_gateway" "gw1" {
   vpc_id = aws_vpc.vpc_1.id
@@ -71,13 +71,13 @@ resource "aws_internet_gateway" "gw1" {
   }
 }
 
-resource "aws_internet_gateway" "gw2" {
-  vpc_id = aws_vpc.vpc_2.id
+# resource "aws_internet_gateway" "gw2" {
+#   vpc_id = aws_vpc.vpc_2.id
 
-  tags = {
-    Name = "${var.gateway_name_2}"
-  }
-}
+#   tags = {
+#     Name = "${var.gateway_name_2}"
+#   }
+# }
 
 resource "aws_route_table" "public_route_table_1" {
   vpc_id = aws_vpc.vpc_1.id
@@ -92,59 +92,152 @@ resource "aws_route_table" "public_route_table_1" {
   }
 }
 
-resource "aws_route_table" "public_route_table_2" {
-  vpc_id = aws_vpc.vpc_2.id
+# resource "aws_route_table" "public_route_table_2" {
+#   vpc_id = aws_vpc.vpc_2.id
 
-  route {
-    cidr_block = var.rt_cidr
-    gateway_id = aws_internet_gateway.gw2.id
-  }
+#   route {
+#     cidr_block = var.rt_cidr
+#     gateway_id = aws_internet_gateway.gw2.id
+#   }
 
-  tags = {
-    Name = "${var.public_rt_name}"
-  }
-}
+#   tags = {
+#     Name = "${var.public_rt_name}"
+#   }
+# }
 
 resource "aws_route_table_association" "rt_associate_public_1" {
-  count = 3
-  subnet_id = aws_subnet.public_subnets_1[count.index].id
+  count          = 3
+  subnet_id      = aws_subnet.public_subnets_1[count.index].id
   route_table_id = aws_route_table.public_route_table_1.id
 }
 
-resource "aws_route_table_association" "rt_associate_public_2" {
-  count = 3
-  subnet_id = aws_subnet.public_subnets_2[count.index].id
-  route_table_id = aws_route_table.public_route_table_2.id
-}
+# resource "aws_route_table_association" "rt_associate_public_2" {
+#   count          = 3
+#   subnet_id      = aws_subnet.public_subnets_2[count.index].id
+#   route_table_id = aws_route_table.public_route_table_2.id
+# }
 
 resource "aws_route_table" "private_route_table_1" {
 
-    vpc_id = aws_vpc.vpc_1.id
+  vpc_id = aws_vpc.vpc_1.id
 
-    tags = {
-        Name = "${var.private_rt_name}"
-    }
-  
+  tags = {
+    Name = "${var.private_rt_name}"
+  }
+
 }
 
-resource "aws_route_table" "private_route_table_2" {
+# resource "aws_route_table" "private_route_table_2" {
 
-    vpc_id = aws_vpc.vpc_2.id
+#   vpc_id = aws_vpc.vpc_2.id
 
-    tags = {
-        Name = "${var.private_rt_name}"
-    }
-  
-}
+#   tags = {
+#     Name = "${var.private_rt_name}"
+#   }
+
+# }
 
 resource "aws_route_table_association" "rt_associate_private_1" {
-  count = 3
-  subnet_id = aws_subnet.private_subnets_1[count.index].id
+  count          = 3
+  subnet_id      = aws_subnet.private_subnets_1[count.index].id
   route_table_id = aws_route_table.private_route_table_1.id
 }
 
-resource "aws_route_table_association" "rt_associate_private_2" {
-  count = 3
-  subnet_id = aws_subnet.private_subnets_2[count.index].id
-  route_table_id = aws_route_table.private_route_table_2.id
+# resource "aws_route_table_association" "rt_associate_private_2" {
+#   count          = 3
+#   subnet_id      = aws_subnet.private_subnets_2[count.index].id
+#   route_table_id = aws_route_table.private_route_table_2.id
+# }
+
+resource "aws_security_group" "application_sec_grp" {
+  name_prefix = var.security_grp_name
+  vpc_id      = aws_vpc.vpc_1.id
+
+  ingress {
+    from_port   = var.ports[0]
+    to_port     = var.ports[0]
+    protocol    = var.protocol
+    cidr_blocks = [var.rt_cidr]
+  }
+
+  ingress {
+    from_port   = var.ports[1]
+    to_port     = var.ports[1]
+    protocol    = var.protocol
+    cidr_blocks = [var.rt_cidr]
+  }
+
+  ingress {
+    from_port   = var.ports[2]
+    to_port     = var.ports[2]
+    protocol    = var.protocol
+    cidr_blocks = [var.rt_cidr]
+  }
+
+  ingress {
+    from_port   = var.ports[3]
+    to_port     = var.ports[3]
+    protocol    = var.protocol
+    cidr_blocks = [var.rt_cidr]
+  }
+
+  egress {
+    from_port   = var.ports[4]
+    to_port     = var.ports[4]
+    protocol    = var.e_protocol
+    cidr_blocks = [var.rt_cidr]
+  }
+}
+
+resource "aws_key_pair" "app_key_pair" {
+  key_name   = var.keypair_name
+  public_key = file(var.keypair_path)
+}
+
+resource "aws_ebs_volume" "ebs_vol" {
+  availability_zone = "${var.aws_region}${var.availability_zones[0]}"
+  size              = var.ebs_vol_size
+  type              = var.ebs_vol_type
+  tags = {
+    Name = var.ebs_vol_name
+  }
+}
+
+resource "aws_instance" "prod_ec2" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public_subnets_1[0].id
+  vpc_security_group_ids = [aws_security_group.application_sec_grp.id]
+  tags = {
+    Name = var.ec2_name
+  }
+  key_name = aws_key_pair.app_key_pair.key_name
+
+  associate_public_ip_address = true
+  disable_api_termination     = false
+
+  connection {
+    type        = var.conn_type
+    user        = var.user
+    private_key = file(var.private_key_path)
+    timeout     = var.ssh_timeout
+    host        = self.public_ip
+  }
+}
+
+resource "aws_volume_attachment" "ebs_Attach" {
+
+  device_name = var.device_name
+  volume_id   = aws_ebs_volume.ebs_vol.id
+  instance_id = aws_instance.prod_ec2.id
+
+}
+
+resource "aws_eip" "ec2_eip" {
+  vpc = true
+}
+
+resource "aws_eip_association" "ec2_eip_association" {
+  instance_id   = aws_instance.prod_ec2.id
+  allocation_id = aws_eip.ec2_eip.id
 }
--- variables.tf
+++ variables.tf
@@ -1,95 +1,196 @@
 variable "aws_region" {
-    type = string
-    description = "AWS region which is closest"
-    default = "us-east-1"
+  type        = string
+  description = "AWS region which is closest"
+  default     = "us-east-1"
 }
 
 variable "aws_profile" {
-    type = string
-    description = "AWS working profile"
-    default = "dev"
+  type        = string
+  description = "AWS working profile"
+  default     = "dev"
 }
 
 variable "vpc_cidr" {
-    type = list(string)
-    description = "The CIDR block of the VPC"
-    default = ["10.0.0.0/16", "10.1.0.0/16"]
+  type        = list(string)
+  description = "The CIDR block of the VPC"
+  default     = ["10.0.0.0/16", "10.1.0.0/16"]
 }
 
 variable "availability_zones" {
-    type = list(string)
-    description = "Subnets availability zones"
-    default = ["a", "b", "c"]
+  type        = list(string)
+  description = "Subnets availability zones"
+  default     = ["a", "b", "c"]
 }
 
 variable "vpc_name_1" {
-    type = string
-    description = "Name of the VPC"
-    default = "vpc_1"
+  type        = string
+  description = "Name of the VPC"
+  default     = "vpc_1"
 }
 
 variable "vpc_name_2" {
-    type = string
-    description = "Name of the VPC"
-    default = "vpc_2"
+  type        = string
+  description = "Name of the VPC"
+  default     = "vpc_2"
 }
 
 variable "public_subnet_name" {
-    type = string
-    description = "Name of the public subnet"
-    default = "prod-public-subnet"
+  type        = string
+  description = "Name of the public subnet"
+  default     = "prod-public-subnet"
 }
 
 variable "private_subnet_name" {
-    type = string
-    description = "Name of the private subnet"
-    default = "prod-private-subnet"
+  type        = string
+  description = "Name of the private subnet"
+  default     = "prod-private-subnet"
 }
 
 variable "gateway_name_1" {
-    type = string
-    description = "Name of the gateway"
-    default = "internet-gateway-1"
+  type        = string
+  description = "Name of the gateway"
+  default     = "internet-gateway-1"
 }
 
 variable "gateway_name_2" {
-    type = string
-    description = "Name of the gateway"
-    default = "internet-gateway-2"
+  type        = string
+  description = "Name of the gateway"
+  default     = "internet-gateway-2"
 }
 
 variable "public_rt_name" {
-    type = string
-    description = "Name of the public route table"
-    default = "prod-public-route-table"
+  type        = string
+  description = "Name of the public route table"
+  default     = "prod-public-route-table"
 }
 
 variable "private_rt_name" {
-    type = string
-    description = "Name of the private route table"
-    default = "prod-private-route-table"
+  type        = string
+  description = "Name of the private route table"
+  default     = "prod-private-route-table"
 }
 
 variable "rt_cidr" {
-    type = string
-    description = "The CIDR block of the route table"
-    default = "0.0.0.0/0"
+  type        = string
+  description = "The CIDR block of the route table"
+  default     = "0.0.0.0/0"
 }
 
 variable "subnet_prefix_1" {
-    type = string
-    description = "prefix of the cidr"
-    default = "10.0."
+  type        = string
+  description = "prefix of the cidr"
+  default     = "10.0."
 }
 
 variable "subnet_prefix_2" {
-    type = string
-    description = "prefix of the cidr"
-    default = "10.1."
+  type        = string
+  description = "prefix of the cidr"
+  default     = "10.1."
 }
 
 variable "subnet_postfix" {
-    type = string
-    description = "postfix of the cidr"
-    default = ".0/24"
+  type        = string
+  description = "postfix of the cidr"
+  default     = ".0/24"
+}
+
+variable "security_grp_name" {
+  type        = string
+  description = "security group name"
+  default     = "prod application"
+}
+
+variable "ports" {
+  type        = list(number)
+  description = "list of ports"
+  default     = [22, 80, 443, 3000, 0]
+}
+
+variable "protocol" {
+  type        = string
+  description = "protocol name"
+  default     = "tcp"
+}
+
+variable "e_protocol" {
+  type        = string
+  description = "egress protocol name"
+  default     = "-1"
+}
+
+variable "keypair_name" {
+  type        = string
+  description = "key-pair name"
+  default     = "app_key_pair"
+}
+
+variable "keypair_path" {
+  type        = string
+  description = "key-pair path"
+  default     = "~/.ssh/public_key.pem"
+}
+
+variable "ebs_vol_size" {
+  type        = number
+  description = "ebs volume size"
+  default     = 50
+}
+
+variable "ebs_vol_type" {
+  type        = string
+  description = "ebs volume type"
+  default     = "gp2"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "ebs instance type"
+  default     = "t2.micro"
+}
+
+variable "conn_type" {
+  type        = string
+  description = "connection type"
+  default     = "ssh"
+}
+
+variable "user" {
+  type        = string
+  description = "user"
+  default     = "ec2-user"
+}
+
+variable "private_key_path" {
+  type        = string
+  description = "path for private key"
+  default     = "~/.ssh/id_rsa"
+}
+
+variable "ssh_timeout" {
+  type        = string
+  description = "timeout for ssh"
+  default     = "2h"
+}
+
+variable "device_name" {
+  type        = string
+  description = "name of device"
+  default     = "/dev/sdh"
+}
+
+variable "ebs_vol_name" {
+  type        = string
+  description = "ebs volume name"
+  default     = "ebs_volume"
+}
+
+variable "ec2_name" {
+  type        = string
+  description = "name of ec2 instance"
+  default     = "prod_ec2"
+}
+
+variable "ami_id" {
+  type        = string
+  description = "ami id for ec2 instance"
 }
